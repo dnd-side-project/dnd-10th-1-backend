2992cd9978bdfb644c0a34146444014adffba691
TLOWAC <leechanghun55@gmail.com>
2024-01-23 18:27:50 +0900

    [KAN-26] :truck: chore(settings): commitlint extends 변경 conventional \> gitmoji

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+        extends: [
+                // emoji 커밋 설정
+                'gitmoji',
+        ],
+        rules: {},
+};
