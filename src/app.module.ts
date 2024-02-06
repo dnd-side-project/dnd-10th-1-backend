6443892dee2437225d53d6e22098d1b25a9b8057
TLOWAC <leechanghun55@gmail.com>
2024-02-06 19:48:35 +0900

    :bug: fix(domain): 미사용 모듈 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,20 @@
 import { Logger, Module } from '@nestjs/common';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
-import { AuthGuard } from '@/common/guards';
 import { LoggingInterceptor } from '@/common/interceptors';
 
-import { AuthModule } from '@/domain/auth/auth.module';
-import { EmailModule } from '@/domain/email/email.module';
 import { EventsModule } from '@/domain/events/events.module';
 import { HealthCheckModule } from '@/domain/health-check/health.module';
-import { UserModule } from '@/domain/user/user.module';
 
 import { SharedServiceModule } from '@/shared-service/shared-service.module';
 
 @Module({
-        imports: [
-                AuthModule,
-                UserModule,
-                HealthCheckModule,
-                EmailModule,
-                SharedServiceModule,
-                EventsModule,
-        ],
+        imports: [HealthCheckModule, SharedServiceModule, EventsModule],
         controllers: [],
         providers: [
                 {
                         provide: APP_INTERCEPTOR,
                         useClass: LoggingInterceptor,
-                },
-                {
-                        provide: APP_GUARD,
-                        useClass: AuthGuard,
                 },
                 Logger,
         ],
