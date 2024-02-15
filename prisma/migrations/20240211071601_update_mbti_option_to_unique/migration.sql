9852d68807f1b8fe811bd7fc88d04f6179179b5b
TLOWAC <leechanghun55@gmail.com>
2024-02-15 22:18:26 +0900

    :bug: fix(settings): 기존 마이그레이션 파일 제거

--- prisma/migrations/20240211071601_update_mbti_option_to_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[mbti]` on the table `GameMbti` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `GameMbti_mbti_key` ON `GameMbti`(`mbti`);
