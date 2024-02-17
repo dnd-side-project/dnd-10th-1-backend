52fdcf0573f78d2ee539593f239bb2aa4c91dccf
TLOWAC <leechanghun55@gmail.com>
2024-02-17 17:24:35 +0900

    :sparkles: feat(domain): game[mbti] - 사용자 mbti 선택 기능 추가

--- src/domain/game/game.gateway.ts
+++ src/domain/game/game.gateway.ts
@@ -14,6 +14,7 @@ import {
 import { GameEvent } from './types/events';
 import console from 'console';
 import { generateRandomNumber } from '@/common/utils';
+import { UserService } from '../user/user.service';
 
 @WebSocketGateway({
         namespace: '/game',
@@ -25,7 +26,10 @@ export class GameEventsGateway implements OnGatewayInit, OnGatewayConnection, On
         @WebSocketServer()
         public server: Server;
 
-        constructor(private gameService: GameService) {}
+        constructor(
+                private userService: UserService,
+                private gameService: GameService,
+        ) {}
 
         // game - 게임 종류 조회하기
         @SubscribeMessage(GameEvent.SELECT_GAME)
@@ -61,23 +65,26 @@ export class GameEventsGateway implements OnGatewayInit, OnGatewayConnection, On
         // @SubscribeMessage(GameEvent.END_GAME)
         // async onEndGame(@ConnectedSocket() client: Socket, @MessageBody() data: any) {}
 
-        // // game[mbti] - 사용자 mbti 선택
-        // @SubscribeMessage(GameEvent.SELECT_MBTI)
-        // async onSelectMbti(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
-        //         // TODO: 사용자의 mbti 정보 저장
-        //         // await this.prismaService.user.update({
-        //         //      data: { mbti: data.mbti, },
-        //         //     where: {
-        //         //         id: data.userId,
-        //         //      }
-        //         // })
+        // game[mbti] - 사용자 mbti 선택
+        @SubscribeMessage(GameEvent.SELECT_MBTI)
+        async onSelectMbti(
+                @ConnectedSocket() client: Socket,
+                @MessageBody() data: { roomId: string; userId: number; mbti: string },
+        ) {
+                const { roomId, userId, mbti } = data;
 
-        //         // TODO: roomID 으로 필터링하여 user.mbti 가 null 인 경우 조회. 0 이면 mbti 결과 화면으로 이동
-        //         // this.server.to(roomId).emit(GameEvent.MOVE_TO_LOADING_ROOM);
+                const _updateUserMbti = await this.userService.updateMbti({ userId, mbti });
+                const mbtiNullCount = await this.userService.countMbtiByRoomId(roomId);
 
-        //         // TODO: 로딩 페이지 이동
-        //         client.emit(GameEvent.MOVE_TO_LOADING_ROOM);
-        // }
+                // 모든 유저 제출 완료시 결과 이벤트 전송
+                if (mbtiNullCount === 0) {
+                        this.server.to(roomId).emit(GameEvent.MOVE_TO_MBTI_RESULT);
+                        return;
+                }
+
+                // 유저별 로딩 페이지 이동
+                client.emit(GameEvent.MOVE_TO_MBTI_LOADING);
+        }
 
         // // game[빈칸주제] - 게임시작
         // @SubscribeMessage(GameEvent.SEND_USER_ANSWER)
