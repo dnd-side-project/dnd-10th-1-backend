05c98bbb9d053ea9e54591d60bb34cfb2df182d3
TLOWAC <leechanghun55@gmail.com>
2024-02-18 11:03:01 +0900

    :bug: fix(domain): gameService 의존성 추가

--- src/domain/game/game.module.ts
+++ src/domain/game/game.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 
 import { UserService } from '../user/user.service';
 import { GameEventsGateway } from './game.gateway';
+import { RoomService } from '../room/room.service';
 
 @Module({
         controllers: [],
-        providers: [GameService, UserService, GameEventsGateway],
+        providers: [GameService, UserService, RoomService, GameEventsGateway],
         exports: [GameService],
 })
 export class GameModule {}
