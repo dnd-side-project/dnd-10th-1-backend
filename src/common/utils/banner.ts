66943f1c7a214e6176827c6542e428ded9d23bba
TLOWAC <leechanghun55@gmail.com>
2024-03-18 03:57:47 +0900

    :recycle: refactor(settings): printEnvBanner 분리 작업

--- src/common/utils/banner.ts
+++ src/common/utils/banner.ts
@@ -1,3 +1,5 @@
+import { ApiConfigService } from '@/shared-service/env';
+import { Table } from 'console-table-printer';
 import gradient from 'gradient-string';
 
 export function printBootBanner() {
@@ -16,5 +18,36 @@ export function printBootBanner() {
                                 
       `);
 
-        return coolString;
+        console.log(coolString);
+}
+
+export function printEnvBanner(config: ApiConfigService) {
+        const p = new Table({
+                title: '환경변수 체크사항',
+                columns: [
+                        { name: 'env_name', alignment: 'right' },
+                        { name: 'env_value', alignment: 'left' },
+                ],
+        });
+
+        p.addRows([
+                {
+                        env_name: 'NODE_ENV',
+                        env_value: `${config.nodeEnv}`,
+                },
+                {
+                        env_name: 'DB_HOST',
+                        env_value: `${config.dbConfig.url}`,
+                },
+                {
+                        env_name: 'SERVER_URL',
+                        env_value: `${config.appConfig.url}`,
+                },
+                {
+                        env_name: 'SWAGGER_URL',
+                        env_value: `${config.appConfig.url}/docs`,
+                },
+        ]);
+
+        p.printTable();
 }
